b52f03297a1f3d3496094f50cf8e4a1337477360
Jegertabt <65051353+Jegertabt@users.noreply.github.com>
2025-09-29 17:14:45 -0400

    Update chain.cpp
    Removal of bignum dependency for openssl compatibility using Inline nthRoot + geometric mean helpers

--- src/chain.cpp
+++ src/chain.cpp
@@ -6,7 +6,9 @@
 #include <chain.h>
 #include "chainparams.h"
 #include "validation.h"
-#include "bignum.h"
+//#include "bignum.h"  // Removed For Openssl Hot Fix
+#include <arith_uint256.h>
+
 
 /* Moved here from the header, because we need auxpow and the logic
    becomes more involved.  */
@@ -268,30 +270,103 @@ arith_uint256 GetPrevWorkForAlgoWithDecay3(const CBlockIndex& block, int algo)
     return arith_uint256(0);
 }
 
-arith_uint256 GetBlockProof(const CBlockIndex& block)
-{
+// Removal of bignum dependency for openssl compatibility
+// Geometric mean of the work done by each algo over the last NUM_ALGOS blocks
+
+// nthRoot helper function
+// Uses bit approximation and then 20 rounds of Newton's method
+static arith_uint256 uint256_nthRoot(const int root, const arith_uint256& bn) {
+    assert(root > 1);
+    if (bn == 0) return arith_uint256(0);
+    assert(bn > 0);
+
+    int nRootBits = (bn.bits() + root - 1) / root;
+    int nStartingBits = std::min(8, nRootBits);
+    arith_uint256 bnUpper = bn;
+    bnUpper >>= (nRootBits - nStartingBits) * root;
+    arith_uint256 bnCur = 0;
+
+    for (int i = nStartingBits - 1; i >= 0; i--) {
+        arith_uint256 bnNext = bnCur + (arith_uint256(1) << i);
+        arith_uint256 bnPower = 1;
+        for (int j = 0; j < root; j++)
+            bnPower *= bnNext;
+        if (bnPower <= bnUpper)
+            bnCur = bnNext;
+    }
+    if (nRootBits == nStartingBits)
+        return bnCur;
+    bnCur <<= (nRootBits - nStartingBits);
+
+    // Newton iterations
+    const arith_uint256 bnRoot = arith_uint256(root);
+    int nTerminate = 0;
+    bool fNegativeDelta = false;
+    for (int it = 0; it < 20; it++) {
+        arith_uint256 bnDenominator = 1;
+        for (int i = 0; i < root - 1; i++)
+            bnDenominator *= bnCur;
+
+        if (bnCur > bn / bnDenominator)
+            fNegativeDelta = true;
+        if (bnCur == bn / bnDenominator)
+            return bnCur;
+
+        arith_uint256 bnDelta;
+        if (fNegativeDelta) {
+            bnDelta = bnCur - bn / bnDenominator;
+            if (nTerminate == 1)
+                return bnCur - 1;
+            fNegativeDelta = false;
+            if (bnDelta <= bnRoot) {
+                bnCur -= 1;
+                nTerminate = -1;
+                continue;
+            }
+            fNegativeDelta = true;
+        } else {
+            bnDelta = bn / bnDenominator - bnCur;
+            if (nTerminate == -1)
+                return bnCur;
+            if (bnDelta <= bnRoot) {
+                bnCur += 1;
+                nTerminate = 1;
+                continue;
+            }
+        }
+        if (fNegativeDelta) {
+            bnCur -= bnDelta / bnRoot;
+        } else {
+            bnCur += bnDelta / bnRoot;
+        }
+        nTerminate = 0;
+    }
+    return bnCur;
+}
+
+// Geometric mean helper
+arith_uint256 GetGeometricMeanPrevWork(const CBlockIndex& block) {
     arith_uint256 nBlockWork = GetBlockProofBase(block);
-    CBigNum bnBlockWork = CBigNum(ArithToUint256(nBlockWork));
     int nAlgo = block.GetAlgo();
-    
-    for (int algo = 0; algo < NUM_ALGOS_IMPL; algo++)
-    {
-        if (algo != nAlgo)
-        {
+
+    // nthRoot(product rule) to avoid overflow
+    nBlockWork = uint256_nthRoot(NUM_ALGOS, nBlockWork);
+
+    for (int algo = 0; algo < NUM_ALGOS_IMPL; algo++) {
+        if (algo != nAlgo) {
             arith_uint256 nBlockWorkAlt = GetPrevWorkForAlgoWithDecay3(block, algo);
-            CBigNum bnBlockWorkAlt = CBigNum(ArithToUint256(nBlockWorkAlt));
-            if (bnBlockWorkAlt != 0)
-                bnBlockWork *= bnBlockWorkAlt;
+            if (nBlockWorkAlt != 0)
+                nBlockWork *= uint256_nthRoot(NUM_ALGOS, nBlockWorkAlt);
         }
     }
-    // Compute the geometric mean
-    CBigNum bnRes = bnBlockWork.nthRoot(NUM_ALGOS);
-    
-    // Scale to roughly match the old work calculation
-    bnRes <<= 8;
-    
-    //return bnRes;
-    return UintToArith256(bnRes.getuint256());
+
+    nBlockWork <<= 8;
+    return nBlockWork;
+}
+
+// Final block proof
+arith_uint256 GetBlockProof(const CBlockIndex& block) {
+    return GetGeometricMeanPrevWork(block);
 }
 
 int64_t GetBlockProofEquivalentTime(const CBlockIndex& to, const CBlockIndex& from, const CBlockIndex& tip, const Consensus::Params& params)
